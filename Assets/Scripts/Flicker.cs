9155f6b0f723f7d954234733f60fb2d1eb9650ff
JennaLeeL <jennalee.lecavalier@gmail.com>
2024-11-12 12:51:59 -0500

    Light flicker

--- Assets/Scripts/Flicker.cs
+++ Assets/Scripts/Flicker.cs
@@ -1,31 +1,106 @@
+using System.Collections;
 using UnityEngine;
 
-public class LightFlicker : MonoBehaviour
+// https://assetstore.unity.com/packages/3d/props/interior/old-torch-203664#content
+
+public class Flickert : MonoBehaviour
 {
-    public Light lightSource;  // The light source to flicker
-    public float minIntensity = 0.5f;  // Minimum light intensity
-    public float maxIntensity = 1.5f;  // Maximum light intensity
-    public float flickerSpeed = 0.1f;  // Speed of flickering effect
+    Vector3 _startPosLight;
+    [Tooltip("The light attached to this script")]
+    public Light lig;
+    [Tooltip("The light color")]
+    public Color colorLight = Color.white;
+    [Space]
+    [Tooltip("The minimum Intensity Light")]
+    public float min = 0.0f;
+    [Tooltip("The maximum Intensity Light")]
+    public float max = 2.0f;
+    [Space(20)]
+    private float _flickIntensity;
+    [Tooltip("The timing of the speed for flick Intensity of the light")]
+    public float timer = 1.0f;
+    [Tooltip("The waiting time for the light to flicker")]
+    public float smooth = 0.1f;
+    [Space(10)]
+    [Header("Set a false movement of the shadow")]
+    [Space(10)]
+    public bool moveShadow = false;
+    [Tooltip("The speed of the movement of light")]
+    public float speedMoveShadow = 1f;
+    [Tooltip("The speed smooth of the movement of light")]
+    public float speedSmoothShadow = 50f;
+    
+    [Header("Stop Flickering After Time")]
+    [Tooltip("Time in seconds after which the light stops flickering")]
+    public float stopAfterTime = 10.0f; // Time after which flickering will stop
+    private float _flickerTimer;
+    private bool _isFlickering = true;  // To control if flickering is active or not
 
-    private float timer = 0f;
-
+    // Start is called before the first frame update
     void Start()
     {
-        if (lightSource == null)
+        if (lig == null)
         {
-            lightSource = GetComponent<Light>();  // Try to get the Light component if not set
+            lig = GetComponent<Light>();
+            _startPosLight = lig.transform.position;
+        }
+
+        _flickerTimer = stopAfterTime; // Set the timer to the stop time
+        StartCoroutine(SmoothFLick());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lig == null)
+            return;
+
+        if (_isFlickering)
+        {
+            StartCoroutine(SmoothFLick());
+        }
+        
+        MoveShadowLight();
+
+        // Countdown the flickering timer
+        _flickerTimer -= Time.deltaTime;
+
+        if (_flickerTimer <= 0 && _isFlickering)
+        {
+            StopFlickering();
         }
     }
 
-    void Update()
+    private IEnumerator SmoothFLick()
     {
-        // Update the timer based on the flicker speed
-        timer += Time.deltaTime * flickerSpeed;
+        if (_isFlickering)
+        {
+            _flickIntensity = Mathf.Lerp(_flickIntensity, (Random.Range(min, max)), timer * Time.smoothDeltaTime);
+            lig.intensity = _flickIntensity;
+            lig.color = colorLight;
+            yield return new WaitForSeconds(smooth);
+        }
+    }
 
-        // Create a random flicker effect by modifying the intensity
-        lightSource.intensity = Mathf.Lerp(minIntensity, maxIntensity, Mathf.PerlinNoise(timer, 0f));
+    void MoveShadowLight()
+    {
+        if (moveShadow)
+        {
+            lig.transform.position = _startPosLight + (Random.insideUnitSphere * speedMoveShadow / speedSmoothShadow);
+        }
+        else
+        {
+            moveShadow = false;
+            lig.transform.position = _startPosLight;
+        }
+    }
 
-        // You can also adjust the flicker more sharply if you want a more random effect
-        // lightSource.intensity = Random.Range(minIntensity, maxIntensity);
+    // This method will stop the flickering and set the light to a fixed state
+    void StopFlickering()
+    {
+        _isFlickering = false;
+        lig.intensity = min;  // Set the light intensity to min or any fixed value you'd prefer
+        // Optionally, you could also turn off the light or change its color
+        // lig.enabled = false;  // This would completely turn off the light.
     }
 }
